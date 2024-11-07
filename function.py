0076bf7bb3caca247931f306f262abb2199f0495
kevincyubahiro <kevincyubahiro250@gmail.com>
2024-11-07 05:37:46 -0800

    f_commit

--- function.py
+++ function.py
@@ -1,9 +1,12 @@
 def greet():
     print("Hello, welcome to Python!")
 greet()  
+
+
 def get_default_value():
     return 42
 
 # Calling the function
 result = get_default_value()
-print(result)  # Output: 42
+print(result) 
+ # Output: 42
